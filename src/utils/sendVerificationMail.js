92c20168b9adecab840c333dd253170ecbe5882f
VTeplov <vteplov@tisbi.ru>
2025-04-19 15:33:09 +0300

    fix mailer

--- src/utils/sendVerificationMail.js
+++ src/utils/sendVerificationMail.js
@@ -1,21 +1,40 @@
-import nodemailer from 'nodemailer';
+// import nodemailer from 'nodemailer';
+
+// export const sendVerificationEmail = async (email, token) => {
+//   const transporter = nodemailer.createTransport({
+//     host: 'smtp.timeweb.ru',
+//     port: 465,
+//     secure: true, // TLS (если используешь порт 465)
+//     auth: {
+//       user: process.env.MAIL_ADDRESS,
+//       pass: process.env.MAIL_PASSWORD,
+//     },
+//   });
+
+//   const verifyLink = `${process.env.SITE_URL}/auth/verify?email=${email}&token=${token}`;
+
+//   await transporter.sendMail({
+//     from: `"Balivito" <${process.env.MAIL_ADDRESS}>`,
+//     to: email,
+//     subject: 'Подтверждение регистрации',
+//     html: `
+//       <h2>Подтверждение почты</h2>
+//       <p>Для активации аккаунта перейдите по ссылке:</p>
+//       <a href="${verifyLink}">${verifyLink}</a>
+//     `,
+//   });
+// };
+
+
+import { Resend } from 'resend';
+
+const resend = new Resend('re_LP6kCWZq_BfnF1ZDfupHgxrM2i61XWetz');
 
 export const sendVerificationEmail = async (email, token) => {
-  const transporter = nodemailer.createTransport({
-    host: 'smtp.gmail.com',
-    port: 465, // было 587
-    secure: true, // было false
-    auth: {
-      user: process.env.MAIL_ADDRESS,
-      pass: process.env.MAIL_PASSWORD,
-    },
-  });
-  
-
   const verifyLink = `${process.env.SITE_URL}/auth/verify?email=${email}&token=${token}`;
 
-  await transporter.sendMail({
-    from: `"Balivito" <${process.env.MAIL_ADDRESS}>`,
+  await resend.emails.send({
+    from: 'onboarding@resend.dev', // разрешённый отправитель Resend
     to: email,
     subject: 'Подтверждение регистрации',
     html: `
