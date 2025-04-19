4a62da112cd31f2dbd3c5db542610f2dc0a972ff
VTeplov <vteplov@tisbi.ru>
2025-04-20 00:50:54 +0300

    fix favorites

--- src/controllers/favorites.js
+++ src/controllers/favorites.js
@@ -21,7 +21,7 @@ export const toggleFavorite = async (req, res) => {
 
     await user.save();
 
-    res.json({ message: index === -1 ? 'Добавлено в избранное' : 'Удалено из избранного', isFavorite: index === -1 });
+    res.json({ message: index === -1 ? 'Добавлено в избранное' : 'Удалено из избранного', isFavorite: index === -1, ad });
   } catch (err) {
     console.error('Ошибка обновления избранного:', err);
     res.status(500).json({ message: 'Ошибка сервера' });
