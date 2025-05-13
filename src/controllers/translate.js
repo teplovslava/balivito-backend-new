7fe654355981345212a27c30c36f6a31ca9874eb
VTeplov <vteplov@tisbi.ru>
2025-05-14 02:37:19 +0300

    fix

--- src/controllers/translate.js
+++ src/controllers/translate.js
@@ -9,7 +9,7 @@ export const translate = async (req, res) => {
 
   try {
     const response = await axios.post(
-      "http://0.0.0.0:5000/translate",
+      "http://libretranslate:5000/translate",
       {
         q: text,
         source: "auto",
