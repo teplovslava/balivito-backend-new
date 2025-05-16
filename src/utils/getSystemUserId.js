c4ece18d08a059cf735f43dbc938396f36e825ab
VTeplov <vteplov@tisbi.ru>
2025-05-16 20:41:23 +0300

    fix

--- src/utils/getSystemUserId.js
+++ src/utils/getSystemUserId.js
@@ -1,3 +1,8 @@
+import dotenv from "dotenv";
+import path from "path";
+
+dotenv.config({ path: path.resolve("/app/.env") });
+
 export function getSystemUserId() {
     console.log(process.env)
     return process.env.SYSTEM_USER_ID;
