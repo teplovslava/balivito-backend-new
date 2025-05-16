ce465a96263fc56340162d3278fb6bb6d99e0f9c
VTeplov <vteplov@tisbi.ru>
2025-05-16 19:58:24 +0300

    debug

--- src/utils/getSystemChat.js
+++ src/utils/getSystemChat.js
@@ -3,6 +3,8 @@ import Chat from "../models/Chat.js";
 export const SYSTEM_USER_ID = process.env.SYSTEM_USER_ID;
 
 export async function getSystemChatForUser(userId) {
+
+    console.log(SYSTEM_USER_ID,userId)
   const chat = await Chat.findOne({
     participants: { $all: [SYSTEM_USER_ID, userId], $size: 2 },
   });
